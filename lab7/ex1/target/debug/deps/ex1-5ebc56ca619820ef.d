2bd917f78372dac5658905d91ffb6f3e8e9eae54
RobertoSpy <robertospiridon1@gmail.com>
2024-11-17 17:34:40 +0200

    lab7

---
+++ lab7/ex1/target/debug/deps/ex1-5ebc56ca619820ef.d
@@ -0,0 +1,5 @@
+C:\Users\Roberto\OneDrive\Desktop\RUST1\lab7\ex1\target\debug\deps\libex1-5ebc56ca619820ef.rmeta: src/main.rs
+
+C:\Users\Roberto\OneDrive\Desktop\RUST1\lab7\ex1\target\debug\deps\ex1-5ebc56ca619820ef.d: src/main.rs
+
+src/main.rs:
---
+++ lab7/ex1/target/debug/ex1.d
@@ -0,0 +1,1 @@
+C:\Users\Roberto\OneDrive\Desktop\RUST1\lab7\ex1\target\debug\ex1.exe: C:\Users\Roberto\OneDrive\Desktop\RUST1\lab7\ex1\src\main.rs
